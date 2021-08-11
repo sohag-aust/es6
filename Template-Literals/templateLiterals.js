534639831bfb90e7928b234598a250899fbcc2c2
sohag-aust <frustratedcoder96@gmail.com>
2021-08-12 00:03:25 +0600

    Template Literals

---
+++ Template-Literals/templateLiterals.js
@@ -0,0 +1,12 @@
+// it is just about using the backtick -> ``
+
+var a = 13;
+var b = 13;
+var name = 'Shohag';
+
+console.log(`My name is ${name} and i'm ${a+b} years old`);
+
+// we can print in newLine too using the template Literals
+console.log(`My
+    name is
+        ${name}`);
